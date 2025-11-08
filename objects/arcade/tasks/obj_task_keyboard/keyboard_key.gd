05cb1e8e34a045fa578eecbb97eeead173a037b0
Zacky <58615455+ZackyGameDev@users.noreply.github.com>
2025-11-08 11:05:48 +0530

    [bedroom] drawn

---
+++ objects/arcade/tasks/obj_task_keyboard/keyboard_key.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var hsp: float
+var vsp: float
+var rotsp_deg: float
+var grv: float
+var hrv: float
+
+func _ready(): 
+	grv = 0.1
+	hrv = 0.001
+	rotsp_deg = randf_range(-10, 10)
+	hsp = randf_range(-2, 2)
+	vsp = randf_range(-4, -1)
+	scale *= randf_range(0.5, 2.0)
+
+func _physics_process(delta: float) -> void:
+	position.x += hsp
+	position.y += vsp
+	vsp += grv
+	hsp = hsp - sign(hsp)*hrv
+	rotation_degrees += rotsp_deg
---
+++ objects/arcade/tasks/obj_task_keyboard/task_keyboard.gd
@@ -0,0 +1,85 @@
+extends Task
+class_name TaskKeyboard
+
+var fuse_time: float = 7.0
+var keys_amount: int = 30
+var shake_intensity: float = 2
+var shake_delta: Vector2 = Vector2(0, 0)
+var critical_threshhold = 0.5
+
+var warning_scene: Resource
+var warning: Node2D
+
+func _ready():
+	warning_scene = preload("res://objects/arcade/tasks/obj_task_keyboard/task_keyboard_warning.tscn")
+	super()
+	attack()
+	hp = 15
+
+func die():
+	if is_instance_valid(warning):
+		warning.queue_free()
+	super()
+
+func spawn_warning() -> Node2D:
+	warning = warning_scene.instantiate()
+	var sprite: Sprite2D = warning.get_node("sprite")
+	sprite.scale = Vector2.ZERO
+	warning.position = pivot_pos
+	sprite.self_modulate.a = 0
+	get_parent().add_child.call_deferred(warning)
+	var tween = get_tree().create_tween()
+	tween.tween_property(sprite, "self_modulate:a", 0.5, fuse_time*critical_threshhold) \
+		.set_ease(Tween.EASE_OUT)
+	tween.parallel().tween_property(sprite, "scale", 3*Vector2.ONE, fuse_time*critical_threshhold)
+	return sprite
+
+func command_player_to_screen_shake():
+	var player: ArcadePlayer = get_tree().current_scene.find_child("player", true)
+	if player == null:
+		print("keyboard did not find players")
+		return
+	player.shake_strength = 5
+
+func attack() -> void:
+	
+	var t: Timer = Timer.new()
+	t.wait_time = fuse_time
+	t.one_shot = true
+	add_child(t)
+	t.start()
+	
+	# blinking
+	var tweak = get_tree().create_tween()
+	tweak.set_loops()
+	tweak.tween_property($sprite, "self_modulate", Color(10, 10, 10), 0.5)
+	tweak.tween_property($sprite, "self_modulate", Color(1, 1, 1), 0.5)
+	
+	while t.time_left > 0:
+		if t.time_left/t.wait_time < critical_threshhold and tweak.is_valid():
+			tweak.kill()
+			var tweak_harder = get_tree().create_tween()
+			tweak_harder.set_loops()
+			tweak_harder.tween_property($sprite, "self_modulate", Color(10, 10, 10), 0.2)
+			tweak_harder.tween_property($sprite, "self_modulate", Color(1, 1, 1), 0.2)
+			spawn_warning()
+			
+		shake_delta = Vector2(
+			randf_range(-shake_intensity, shake_intensity), 
+			randf_range(-shake_intensity, shake_intensity)
+		) * (1 - t.time_left/t.wait_time)
+		await get_tree().process_frame
+	
+	var scene = preload("res://objects/arcade/tasks/obj_task_keyboard/keyboard_key.tscn")
+	for i in range(keys_amount):
+		var key: Node2D = scene.instantiate()
+		key.global_position = global_position
+		get_parent().add_child(key)
+	
+	command_player_to_screen_shake()
+	warning.queue_free()
+	queue_free()
+
+func _physics_process(_delta: float) -> void:
+	super(_delta)
+	position += shake_delta
