3110cf46a8e8e3abe5aa1e84598678fd2d5b58e9
Zacky <58615455+ZackyGameDev@users.noreply.github.com>
2025-11-09 21:48:24 +0530

    [twek]

--- rooms/rm_title/rm_title.gd
+++ rooms/rm_title/rm_title.gd
@@ -5,11 +5,14 @@ func _ready() -> void:
 	chakra_tween.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 	chakra_tween.tween_property($chakra, "self_modulate:a", 0.0, 1.0)
 	chakra_tween.tween_property($chakra, "self_modulate:a", 0.2, 1.0)
-
 	$endings.text = "Endings found: (" + str(SaveSystem.load_game()[1]) + "/2)"
 	if SaveSystem.load_game()[1] == 2:
 		$endings.text = $endings.text + "\n Thank you, for playing my game."
 
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 
 func _on_play_button_activated(_data: Variant) -> void:
 	get_tree().change_scene_to_file("res://rooms/rm_cutscene1/rm_custcene1.tscn")
